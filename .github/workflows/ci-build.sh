d82cebf817bf7f4b44c215b6bcbe4eaff70db0d5
mohamedsamir759 <https://github.com/mohamedsamir759>
2024-10-13 15:10:47 +0300

    deploy my image on k8s cluster

--- .github/workflows/ci-build.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/mohamedsamir759/github-actions.git
-cd github-actions/
-
-
-docker build -t myapp:v${BUILD_NUMBER} .
-docker run -d --name test${BUILD_NUMBER} -p 80:80 myapp:v${BUILD_NUMBER}
-docker stop test
-docker rm test
-
